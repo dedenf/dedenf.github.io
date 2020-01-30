c51b596db2f3ca9a43b1754cf5a8ed7707ecde18
Deden Fathurahman <dedenf@gmail.com>
2020-01-30 20:53:06 +0700

    experiment with callout

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200130193154";
+var CACHE_NAME = "pixyll2-20200130205231";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202001301931",
+        "/css/pixyll.css?202001302052",
         "/"
       ]);
     })
