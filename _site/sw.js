6cc305bb72d98160e04f2ee0c340a40a50ba83e7
Deden Fathurahman <dedenf@gmail.com>
2020-03-13 13:07:58 +0700

    update font

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200311160500";
+var CACHE_NAME = "pixyll2-20200313130721";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202003111605",
+        "/css/pixyll.css?202003131307",
         "/"
       ]);
     })
