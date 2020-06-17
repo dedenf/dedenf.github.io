5203db2aefb9472ce94e120f96ba6520ed65f5ff
Deden Fathurahman <dedenf@gmail.com>
2020-06-17 14:38:34 +0700

    new post, darah murni, and how i hated it

--- _site/sw.js
+++ _site/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200608220018";
+var CACHE_NAME = "pixyll2-20200617143739";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202006082200",
+        "/css/pixyll.css?202006171437",
         "/"
       ]);
     })
